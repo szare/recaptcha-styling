54f1ea03cda7bb42f4014860f96981c40740899f
Salman Zare <salman.zare@optus.com.au>
2019-03-06 20:26:01 +1100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div class="g-recaptcha" data-sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
